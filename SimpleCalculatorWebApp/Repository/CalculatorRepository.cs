2013b199dbacd6b6698fef23e17d7f238bd6b9fa
Minardsa <alphaomega_41@hotmail.com>
2016-10-24 23:23:39 -0400

    Add files via upload

---
+++ SimpleCalculatorWebApp/Repository/CalculatorRepository.cs
@@ -0,0 +1,43 @@
+﻿using SimpleCalculatorWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCalculatorWebApplication.Repository
+{
+    public class CalculatorRepository
+    {
+        public string CalculateOperands(FormulaModel fm)
+        {
+            double result = 0.0;
+
+            try
+            {
+                switch (fm.Operator)
+                {
+                    case "+":
+                        result = fm.Operand1 + fm.Operand2;
+                        break;
+                    case "-":
+                        result = fm.Operand1 - fm.Operand2;
+                        break;
+                    case "*":
+                        result = fm.Operand1 * fm.Operand2;
+                        break;
+                    case "/":
+                        result = fm.Operand1 / fm.Operand2;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+
+            return result.ToString();
+        }
+    }
+}

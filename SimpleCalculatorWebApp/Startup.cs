c27260bcdf755050a18cb572f08dfe9d9b8060f1
Minardsa <alphaomega_41@hotmail.com>
2016-10-24 22:45:03 -0400

    Add files via upload

---
+++ SimpleCalculatorWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleCalculatorWebApplication.Startup))]
+namespace SimpleCalculatorWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

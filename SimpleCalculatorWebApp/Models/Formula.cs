52c19062ec5afe3a4b80cd409541fefe315fbadd
Minardsa <alphaomega_41@hotmail.com>
2016-10-24 23:21:01 -0400

    Add files via upload

---
+++ SimpleCalculatorWebApp/Models/Formula.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SimpleCalculatorWebApplication.Models
+{
+    public class FormulaModel : IFormula
+    {
+        [Display(Name = "Operand 1")]
+        [Required(ErrorMessage = "The first operand is required")]
+        public double Operand1 { get; set; }
+
+        [Display(Name = "Operand 2")]
+        [Required(ErrorMessage = "The second operand is required")]
+        public double Operand2 { get; set; }
+
+        [Required(ErrorMessage = "The operator is required")]
+        public string Operator { get; set; }
+    }
+}
---
+++ SimpleCalculatorWebApp/Models/IFormula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorWebApplication.Models
+{
+    interface IFormula
+    {
+        double Operand1 { get; set; }
+        double Operand2 { get; set; }
+        string Operator { get; set; }
+    }
+}

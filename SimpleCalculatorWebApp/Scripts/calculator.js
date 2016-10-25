9ad8c8cb3842ba3f9d23281c0272758a2f925a1a
Minardsa <alphaomega_41@hotmail.com>
2016-10-24 23:24:47 -0400

    Add files via upload

---
+++ SimpleCalculatorWebApp/Scripts/calculator.js
@@ -0,0 +1,29 @@
+﻿function loadClickedNumber(num) {
+
+    if (document.getElementById("textBoxActive").value == "TB1")
+        document.getElementById("Text1").value += num.trim();
+    else if (document.getElementById("textBoxActive").value == "TB2")
+        document.getElementById("Text2").value += num.trim();
+
+}
+
+function setActiveTextBox(operand) {
+
+    if (operand == "Op1")
+        document.getElementById("textBoxActive").value = "TB1"
+    else if (operand == "Op2")
+        document.getElementById("textBoxActive").value = "TB2"
+
+}
+
+function clearAllFields() {
+
+    document.getElementById("Text1").value = null;
+    document.getElementById("Text2").value = null;
+
+    document.getElementById("Add").checked = null; 
+    document.getElementById("Sub").checked = null;
+    document.getElementById("Mlt").checked = null;
+    document.getElementById("Div").checked = null;
+
+}
